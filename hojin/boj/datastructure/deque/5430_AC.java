7bb225f4672aae29371c8881bbf408d2e36ce19e
안호진 <asdv94@naver.com>
2022-02-13 01:31:00 +0900

    [BOJ] AC / 골드 5 / 2시간 https://www.acmicpc.net/problem/5430 나름의 refactor 버전 + 주석 추가

--- hojin/boj/datastructure/deque/5430_AC.java
+++ hojin/boj/datastructure/deque/5430_AC.java
@@ -1,16 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class BOJ5430 {
 
 	static BufferedReader br;
 	static StringBuilder sb;
-	static int T, tc, n, i, j, cnt;
+	static int T, tc, n, i, j;
 	static String funcP, tmpStr, tmpStrList[];
-	static Deque<Integer> dq;
-	static boolean flag;
+	static ArrayDeque<Integer> dq;
+	static boolean err, rvrs;
 
 	public static void main(String[] args) throws Exception {
 		br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,33 +17,57 @@ public class BOJ5430 {
 		T = Integer.parseInt(br.readLine());
 		dq = new ArrayDeque<Integer>();
 		for (tc = 0; tc < T; tc++) {
+			// 각 case별 초기화
 			dq.clear();
+			err = rvrs = false;
+			// 함수 P String 받기
 			funcP = br.readLine();
+			// 배열 내 개수
 			n = Integer.parseInt(br.readLine());
+			// 문자열 처리
+			// 한줄 받고
 			tmpStr = br.readLine();
+			// substring을 통해 양 끝에 []제거
 			tmpStr = tmpStr.substring(0, tmpStr.length() - 1).substring(1);
+			// ,을 기준으로 StringArray 생성
 			tmpStrList = tmpStr.split(",");
+			// 각 숫자들을 dq에 넣어주기
 			for (j = 0; j < n; j++) {
 				dq.offer(Integer.parseInt(tmpStrList[j]));
 			}
-			flag = true;
-			cnt = 0;
+			// funcP 실행
 			for (j = 0; j < funcP.length(); j++) {
 				switch (funcP.charAt(j)) {
 				case 'R':
-					cnt++;
+					// reverse 여부 check
+					if (rvrs)
+						rvrs = false;
+					else
+						rvrs = true;
 					break;
 				case 'D':
-					delete();
+					// error check 하기
+					if (!err) {
+						if (rvrs) {
+							if (dq.pollLast() == null) {
+								err = true;
+								sb.append("error");
+							}
+						} else {
+							if (dq.poll() == null) {
+								err = true;
+								sb.append("error");
+							}
+						}
+					}
 					break;
 				}
 			}
-			if (flag) {
-				if (dq.isEmpty())
-					sb.append("[]");
-				else {
-					sb.append("[");
-					if (cnt % 2 == 1) {
+			// err가 아닐 경우 []를 포함한 String 반환해야 함
+			if (!err) {
+				sb.append("[");
+				if (!dq.isEmpty()) {
+					if (rvrs) {
 						while (!dq.isEmpty()) {
 							sb.append(dq.pollLast()).append(",");
 						}
@@ -54,25 +77,12 @@ public class BOJ5430 {
 						}
 					}
 					sb.setLength(sb.length() - 1);
-					sb.append("]");
 				}
+				sb.append("]");
 			}
 			sb.append("\n");
 		}
+		br.close();
 		System.out.println(sb);
-	}
-
-	private static void delete() {
-		if (cnt % 2 == 0 && flag) {
-			if (dq.poll() == null) {
-				flag = false;
-				sb.append("error");
-			}
-		} else if (cnt % 2 == 1 && flag) {
-			if (dq.pollLast() == null) {
-				flag = false;
-				sb.append("error");
-			}
-		}
 	}
 }
