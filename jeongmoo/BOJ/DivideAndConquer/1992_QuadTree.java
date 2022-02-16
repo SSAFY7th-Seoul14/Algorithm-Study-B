295eee7c9c8a6309d970ddd4c21d79c4d3796726
우정무 <app.le@kakao.com>
2022-02-16 20:49:59 +0900

    [BOJ] 쿼드트리 / 실버1 / 1시간 / 재귀 개선

--- jeongmoo/BOJ/DivideAndConquer/1992_QuadTree.java
+++ jeongmoo/BOJ/DivideAndConquer/1992_QuadTree.java
@@ -1,62 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
-// 백준1992번. 쿼드 트리
+// 백준 1992번. 쿼드트리
 public class BOJ1992_QuadTree {
 	static StringBuilder sb = new StringBuilder();
-	static char[][] map;
-	static int n;
+	static int[][] map;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		
-		map = new char[n][n];
+		// 입력
+		map = new int[n][n];
 		for (int i = 0; i < n; i++) {
-			map[i] = br.readLine().toCharArray();
+			String line = br.readLine();
+			for (int j = 0; j < n; j++) {
+				map[i][j] = line.charAt(j) - '0';
+			}
 		}
 		
-		sb.append(quadtree(n, 0, 0));
+		// 처리
+		quadTree(n, 0, 0);
+		
+		
+		// 출력
 		System.out.println(sb);
 	}
 	
-	static String quadtree(int size, int x, int y) {
-		int nextSize = size/2;
-		String first;
-		String second;
-		String third;
-		String fourth;
-		if (size == 2) {
-			first = Character.toString(map[y][x]); 
-			second = Character.toString(map[y][x+nextSize]); 
-			third = Character.toString(map[y+nextSize][x]); 
-			fourth = Character.toString(map[y+nextSize][x+nextSize]); 
-		} else {
-			first = quadtree(nextSize, x, y);
-			second = quadtree(nextSize, x+nextSize, y);
-			third = quadtree(nextSize, x, y+nextSize);
-			fourth = quadtree(nextSize, x+nextSize, y+nextSize);
+	public static void quadTree(int size, int x, int y) {		
+		int half = size/2;
+		boolean same = true;
+ outer: for(int i=x; i<x+size; i++) {
+			for(int j=y; j<y+size; j++) {
+				if(map[x][y] != map[i][j]) {
+					same = false;
+					break outer;
+				}
+			}
 		}
+ 
+ 		if (same || size == 1) {
+ 			sb.append(map[x][y]);
+ 			return;
+ 		}
 		
-		// 반례
-		// 0101
-		// 0000
-		// 0101
-		// 0000 의 경우 정답 ((0100)(0100)(0100)(0100))이 나와야하므로 같은지 체크
-		boolean allSame = true;
-		char temp = first.charAt(0);
-		for (int i = 1; i < first.length(); i++) {
-			if (first.charAt(i) != temp) {
-				allSame = false;
-				break;
-			}			
-		}
-		
-		if (first.equals(second) && second.equals(third) && third.equals(fourth) && allSame)
-			return first;
-		else
-			return "("+first + second + third + fourth+")";
+		sb.append("(");
+		quadTree(half, x, y);
+		quadTree(half, x, y+half);
+		quadTree(half, x+half, y);
+		quadTree(half, x+half, y+half);
+		sb.append(")");
 	}
 }
