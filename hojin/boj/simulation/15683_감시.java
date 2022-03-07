f7a96377f3fde631b7c6b9726ca27ddb90259fbf
HojinAn <asdv94@naver.com>
2022-03-08 01:25:40 +0900

    [BOJ] 감시 / G5 / 5시간?🤦‍ https://www.acmicpc.net/problem/15683  이것도 어찌보면 되게 규격화 되어 있는 유형 같은데 매번 새롭게 생각해서 푸는 것 같아서 고민입니다  족히 5시간은 걸린듯...  접근법부터 잘못된 문제 같습니다.  그리디처럼 생각하고 접근했으나 잘못된 로직 -> 5는 미리 다 펼쳐놓은 다음에 각 cctv 번호 별로 돌려주면 되겠지 생각하고 접근. 그래서 무슨 번호별로 위치를 저장해놓는 ArrayList 2차원 배열을 구현해서 해보려고 했음...  이걸 구현하는데에도 시간이 오래 걸렸지만(구현 능력이 떨어짐을 방증), 다시 생각해보니 분명 그리디적으로 해결 되지 않는 문제인데 여기서 시간 소요가 너무 컸습니다. 각 cctv의 최선이 전체의 최선이라는 보장이 없는 문제였음에도... 우선 여기에서 시간을 오래 소요했습니다.  그리디 문제에 자주 등장하는 키워드들이 뭐가 있는지 생각해봐야겠습니다. 괜히 요령 피우려다가 시간만 다 버린 문제라 할 수 있겠습니다.  처음부터 계속 접근이 잘못되다보니 족히 3~4시간은 세부적인 경우의 수 따지다가 버렸습니다.  두번째 로직 설계 할 때는 지금은 왜 그랬는지는 모르겠지만 집중력이 떨어져서인지 각 cctv마다 독립적으로 동작해야한다는 사실을 망각한채 cctv 번호 별로 동작하는 코드를 짰기 때문에 다시 수정하는데에도 시간이 오래 걸렸습니다.  분명 bfs를 맞게 짠거 같은데 답이 틀려서 애먹었습니다.각 cctv 별로 비추는 영역 표시를 다르게 했어야 했는데, 틀릴 수 밖에 없는 접근법이었던 것 같습니다.  또 같은 번호의 cctv라고 해도 같은 방향을 비춘다는 보장이 없는데, 이 로직으로 설계하는 과정에서 같은 번호끼리 같은 방향만 비추게하는 실수도 생겼습니다.  각 카메라가 비추는 영역 표시가 구분되어야 할 텐데, 아마도 코드를 몇 줄 더 쓰는게 귀찮다고 같은 번호끼리 같은 번호로 비추는 것으로 작성을 해버린 것 같습니다.  결과적으로 이 부분을 수정하는 아이디어를 떠올리는 것에도 시간이 많이 소요됐습니다.  로직 설계 자체가 잘못 됐다는 걸 조금 더 일찍 깨달았더라면..  결국에 모든 문제를 스스로 깨닫게 되면서 각 카메라 별로 mode가 독립적으로 작동하도록 수정하고, 각 카메라가 비추는 영역도 독립적으로 구분해주고, 모든 경우의 수를 거친 후에 0을 다시 탐색하는 방향으로 수정한 후에 겨우 맞출 수 있었습니다.  태선님 코드를 좀 훔쳐봤는데(?) 태선님처럼 각 경우의 수를 다 나누고 beam 쏘는 작업 자체를 mode별로 나눠서 각 cctv에 맞게 사용 했으면 훨씬 금방 풀었을텐데, 저는 이상하게 (요령 피운다고), 어쭙잖게 module화 한다고 하다보니 오히려 이상하게 따져줘야 하는 경우의수가 너무 많아져서(결국 틀릴거면서) 따져주고 디버깅하다가 시간이 엄청나게 오래 걸렸습니다.  요령 피우지 않고 그냥 묵묵하게 브투트 포스를 일단 생각하고 짜는 훈련을 해야겠습니다. 어제부터 오늘까지 느낀점이 많은 문제입니다. 연습도 어쩌다 보니 많이 한 거 같긴 합니다. 하하...  + 문제에서 의도한건지는 모르겠지만 카메라 번호가 주어져서 각 카메라가 독립 되어 있다는 사실을 헷갈렸던 것 같습니다.

--- hojin/boj/simulation/15683_감시.java
+++ hojin/boj/simulation/15683_감시.java
@@ -21,14 +21,7 @@ public class BOJ15683_감시 {
 		// cctv의 위치 저장해놓을 배열 8개를 넘지 않는다
 		cctvLoc = new int[8][];
 
-		// 이하 코드 주석들은 말도 안되는 로직 설계의 처절한 흔적...
-//		cctvLoc = new ArrayList[6];
-//		for (int i = 0; i <= 5; i++) {
-//			cctvLoc[i] = new ArrayList<int[]>();
-//		}
-
 		min = Integer.MAX_VALUE;
-
 		cctvCnt = 0;
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -39,7 +32,6 @@ public class BOJ15683_감시 {
 				case 6:
 					break;
 				default:
-//					cctvLoc[temp].add(new int[] { i, j });
 					// 각 cctv가 위치한 location과 몇번 cctv인지 저장. r, c, cctvNo
 					cctvLoc[cctvCnt++] = new int[] { i, j, temp };
 					break;
@@ -49,18 +41,15 @@ public class BOJ15683_감시 {
 		}
 		br.close();
 
-		// cctv5 일단 세팅해놓으려 했던 흔적...
-//		for (int i = 0; i < cctvLoc[5].size(); i++) {
-//			cctv((int[]) cctvLoc[5].get(i), 0, 5);
-//		}
-
 		// cctv의 중요도는 숫자가 커질 수록 높다. 따라서 큰 번호가 더 나중에 비추도록 함으로 영역 확보 안정성 가져가기
+		// 5가 먼저 비추고 2가 비춘다면 map을 reset 할 때 2가 비췄던 자리 먼저 갱신하면서 5의 흔적이 남지 않고 0으로 바뀌어버려서 오류가 발생한다.
 		Arrays.sort(cctvLoc, 0, cctvCnt, (a, b) -> {
 			return a[2] - b[2];
 		});
 
 		// dfs를 이용해 완전 탐색 수행
 		for (int i = 0; i < 4; i++) {
+			// 0번째 카메라부터 dfs 수행
 			dfs(0, i);
 		}
 
@@ -74,17 +63,14 @@ public class BOJ15683_감시 {
 			return;
 		}
 
-		// 여기가 문제였음. 2번재 설계때 같은 번호는 반드시 같은 mode로 동작하도록 잘못 짠 코드...
-//		for (int i = 0; i < cctvLoc[cctvNo].size(); i++) {
-//			cctv((int[]) cctvLoc[cctvNo].get(i), mode, cctvNo);
-//		}
-		
-		int nthIlluminating = nthCctv;
-		cctv(cctvLoc[nthCctv], mode, nthIlluminating);
+		// n번째 카메라가 비추는 영역을 구분해주기 위해 7을 더한다.
+		int nthCctvBeam = nthCctv + 7;
+		// n번째 카메라 작동시키기
+		cctvOperation(cctvLoc[nthCctv], mode, nthCctvBeam);
 		for (int i = 0; i < 4; i++) {
 			dfs(nthCctv + 1, i);
 		}
-		reset(nthIlluminating);
+		reset(nthCctvBeam);
 	}
 
 	private static void countZero() {
@@ -95,22 +81,20 @@ public class BOJ15683_감시 {
 					++cnt;
 			}
 		}
-		if (cnt < min) {
-			min = cnt;
-		}
+		min = Math.min(min, cnt);
 	}
 
-	private static void reset(int nthIlluminating) {
+	private static void reset(int nthCctvBeam) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				if (map[i][j] == nthIlluminating) {
+				if (map[i][j] == nthCctvBeam) {
 					map[i][j] = 0;
 				}
 			}
 		}
 	}
 
-	private static void cctv(int[] locInfo, int mode, int nThIlluminating) {
+	private static void cctvOperation(int[] locInfo, int mode, int nthCctvBeam) {
 		int cctvLocR = locInfo[0];
 		int cctvLocC = locInfo[1];
 		int cctvNo = locInfo[2];
@@ -122,7 +106,7 @@ public class BOJ15683_감시 {
 
 			while (rangeCheck(nR, nC)) {
 				if (map[nR][nC] == 0) {
-					map[nR][nC] = nThIlluminating;
+					map[nR][nC] = nthCctvBeam;
 				}
 				nR += delta[i][0];
 				nC += delta[i][1];
@@ -159,38 +143,5 @@ public class BOJ15683_감시 {
 			return true;
 		return false;
 	}
-
-	// 처음에는 이렇게 각 cctv 별로 많이 비출수 있는 놈부터 최대로 많이 비추는 영역을 체크한 후에 cctv 비추는 영역 표시를 하려고
-	// 했습니다만.. 잘못된 설계
-//	private static void cctv(int[] loc, int cctvNo) {
-//		int mode = cctvCheck(loc, cctvNo);
-//		cctv(loc, mode, cctvNo);
-//	}
-//
-//	private static int cctvCheck(int[] loc, int cctvNo) {
-//		int max = Integer.MIN_VALUE;
-//		int mode = 0;
-//		for (int i = 0; i < 4; i++) {
-//			int cnt = 0;
-//			for (int j = 0; j < 4; j++) {
-//				if (modeNo(cctvNo, i, j))
-//					continue;
-//				int nR = loc[0] + delta[j][0];
-//				int nC = loc[1] + delta[j][1];
-//
-//				while (rangeCheck(nR, nC)) {
-//					if (map[nR][nC] == 0)
-//						++cnt;
-//					nR += delta[j][0];
-//					nC += delta[j][1];
-//				}
-//			}
-//			if (cnt > max) {
-//				max = cnt;
-//				mode = i;
-//			}
-//		}
-//		return mode;
-//	}
 
 }
