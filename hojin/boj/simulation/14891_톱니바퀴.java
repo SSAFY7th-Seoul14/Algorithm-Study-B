43cb61bfaab0cea6189257d326efa26b4378c905
안호진 <asdv94@naver.com>
2022-03-13 00:05:36 +0900

    [BOJ] 톱니바퀴 / G5 / 1시간 https://www.acmicpc.net/problem/14891 최대한 정리해본 코드

--- hojin/boj/simulation/14891_톱니바퀴.java
+++ hojin/boj/simulation/14891_톱니바퀴.java
@@ -3,9 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ14891_톱니바퀴 {
 	static class Command {
-		// 회전 시킨 번호, 방향은 1이면 시계, -1이면 반시계
+		// 회전 시킬 톱니 번호, 방향은 1이면 시계, -1이면 반시계
 		int gearNo, rotDir;
 
 		public Command(int gearNo, int rotDir) {
@@ -21,10 +21,9 @@ public class Main {
 		StringTokenizer st;
 		// 12시부터 시계방향, n극 0, s극 1
 		gear = new char[5][];
-		gear[1] = br.readLine().toCharArray();
-		gear[2] = br.readLine().toCharArray();
-		gear[3] = br.readLine().toCharArray();
-		gear[4] = br.readLine().toCharArray();
+		for (int i = 1; i <= 4; i++) {
+			gear[i] = br.readLine().toCharArray();
+		}
 		// 회전횟수 1<=k<=1
 		int k = Integer.parseInt(br.readLine());
 		Command[] commands = new Command[k];
@@ -35,136 +34,66 @@ public class Main {
 		}
 		// target이 되는 방향은 각각 다 12시, 1번 3시, 2번 3시 9시, 3번 3시 9시, 4번 3시 9시
 		int[] north = new int[5];
+		// 비교하고 돌아가는지 여부를 기억해줄 compare 배열
 		boolean[] compare = new boolean[4];
 		for (int i = 0; i < k; i++) {
 			for (int j = 1; j <= 3; j++) {
-				// 1번의 3시와 2번의 9시 비교
-				// 2번의 3시와 3번의 9시 비교
-				// 3번의 3시와 4번의 9시 비교
-				// 3시는 north + 2 % 8
-				// 9시는 north + 6 % 8
+				// 1번의 3시와 2번의 9시 비교, 2번의 3시와 3번의 9시 비교, 3번의 3시와 4번의 9시 비교
+				// 3시는 north + 2 % 8, 9시는 north + 6 % 8
 				compare[j] = (gear[j][(north[j] + 2) % 8] != gear[j + 1][(north[j + 1] + 6) % 8]);
 			}
 			int curNo = commands[i].gearNo;
 			int dirNo = commands[i].rotDir;
+			north[curNo] += 8 - dirNo;
 			switch (curNo) {
 			case 1:
-				if (dirNo != 1) {
-					north[1] += 1;
-				} else {
-					north[1] += 7;
-				}
 				if (compare[1]) {
-					if (dirNo != 1) {
-						north[2] += 7;
-					} else {
-						north[2] += 1;
-					}
+					north[2] += 8 + dirNo;
 					if (compare[2]) {
-						if (dirNo != 1) {
-							north[3] += 1;
-						} else {
-							north[3] += 7;
-						}
+						north[3] += 8 - dirNo;
 						if (compare[3]) {
-							if (dirNo != 1) {
-								north[4] += 7;
-							} else {
-								north[4] += 1;
-							}
+							north[4] += 8 + dirNo;
 						}
 					}
 				}
 				break;
 			case 2:
-				if (dirNo != 1) {
-					north[2] += 1;
-				} else {
-					north[2] += 7;
-				}
 				if (compare[1]) {
-					if (dirNo != 1) {
-						north[1] += 7;
-					} else {
-						north[1] += 1;
-					}
+					north[1] += 8 + dirNo;
 				}
 				if (compare[2]) {
-					if (dirNo != 1) {
-						north[3] += 7;
-					} else {
-						north[3] += 1;
-					}
+					north[3] += 8 + dirNo;
 					if (compare[3]) {
-						if (dirNo != 1) {
-							north[4] += 1;
-						} else {
-							north[4] += 7;
-						}
+						north[4] += 8 - dirNo;
 					}
 				}
 				break;
 			case 3:
-				if (dirNo != 1) {
-					north[3] += 1;
-				} else {
-					north[3] += 7;
-				}
 				if (compare[3]) {
-					if (dirNo != 1) {
-						north[4] += 7;
-					} else {
-						north[4] += 1;
-					}
+					north[4] += 8 + dirNo;
 				}
 				if (compare[2]) {
-					if (dirNo != 1) {
-						north[2] += 7;
-					} else {
-						north[2] += 1;
-					}
+					north[2] += 8 + dirNo;
 					if (compare[1]) {
-						if (dirNo != 1) {
-							north[1] += 1;
-						} else {
-							north[1] += 7;
-						}
+						north[1] += 8 - dirNo;
 					}
 				}
 				break;
 			case 4:
-				if (dirNo != 1) {
-					north[4] += 1;
-				} else {
-					north[4] += 7;
-				}
 				if (compare[3]) {
-					if (dirNo != 1) {
-						north[3] += 7;
-					} else {
-						north[3] += 1;
-					}
+					north[3] += 8 + dirNo;
 					if (compare[2]) {
-						if (dirNo != 1) {
-							north[2] += 1;
-						} else {
-							north[2] += 7;
-						}
+						north[2] += 8 - dirNo;
 						if (compare[1]) {
-							if (dirNo != 1) {
-								north[1] += 7;
-							} else {
-								north[1] += 1;
-							}
+							north[1] += 8 + dirNo;
 						}
 					}
 				}
 				break;
 			}
-			north[4] %= 8;
-			north[3] %= 8;
-			north[2] %= 8;
-			north[1] %= 8;
+			for (int j = 1; j <= 4; j++) {
+				north[j] %= 8;
+			}
 		}
 		int sum = 0;
 		for (int i = 1; i <= 4; i++) {
