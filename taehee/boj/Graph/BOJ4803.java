83d72124e74fe8f305b02a7dc79f4e2268ced8d7
kotaehee <koxogml@naver.com>
2022-03-13 15:02:31 +0900

    [BOJ] 4803 트리 / G4 / https://www.acmicpc.net/problem/4803

--- taehee/boj/Graph/BOJ4803.java
+++ taehee/boj/Graph/BOJ4803.java
@@ -7,7 +7,7 @@ public class Main {
 
 	static int[] roots;
 	static int[] tree;
-	
+
 	public static void main(String args[]) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int tc = 1;
@@ -16,28 +16,31 @@ public class Main {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
-			
+
 			if(n==0 && m==0) break;
-			
+
 			tree = new int[n+1];
 			roots = new int[n+1];
 			for (int i = 1; i < n+1; i++) {
 				roots[i] = i;
 			}
-			
+
 			for (int i = 0; i < m; i++) {
 				st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 				union(from,to);
 			}
-			
+			for (int i = 1; i < n+1; i++) {
+				roots[i] = find(i);
+			}
+
 			Set<Integer> set = new HashSet<>();
 			for (int i = 1; i < n+1; i++) {
 				if(roots[i] != 0) set.add(roots[i]);
 			}
 			int trees = set.size();
-			
+
 			if(trees == 0) System.out.println("Case "+tc++ +": No trees.");
 			else if(trees == 1) System.out.println("Case "+tc++ +": There is one tree.");				
 			else System.out.println("Case "+tc++ +": A forest of "+trees+" trees.");
@@ -47,7 +50,7 @@ public class Main {
 	private static void union(int from, int to) {
 		int a = find(from);
 		int b = find(to);
-		
+
 		if(a==b) { //같으면 사이클 생김
 			for (int i = 1; i < roots.length; i++) {
 				if(roots[i] == a) roots[i] = 0;
@@ -65,5 +68,5 @@ public class Main {
 		if(roots[a] == a) return a;
 		return roots[a] = find(roots[a]);
 	}
-	
-}
+
+} 
