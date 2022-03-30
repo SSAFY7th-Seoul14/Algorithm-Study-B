afdb1f2afc7a513def6f945301c65e8acf7c4ef2
kotaehee <koxogml@naver.com>
2022-03-30 13:16:49 +0900

    [BOJ] 2133 타일 채우기 / G5 / https://www.acmicpc.net/problem/2133

--- taehee/boj/DP/2133.java
+++ taehee/boj/DP/2133.java
@@ -7,15 +7,14 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	int n = Integer.parseInt(st.nextToken());
+    	int n = Integer.parseInt(br.readLine());
     	
     	int[] dp = new int[31];
     	dp[2] = 3;
     	for (int i = 4; i < n+1; i++) {
 			if(i%2==0) {
-				dp[i] = 3 * dp[i-2] + 2;
-				for (int j = 2; j <= i-4; j = j+2) {
+				dp[i] = 3 * dp[i-2] + 2; //이전거 곱하기 3 + 각경우마다 2가지추가(경계깨면서 놓는경우)
+				for (int j = 2; j <= i-4; j = j+2) { //[8] = [2]*[6] + [4]*[4]
 					dp[i] += dp[j]*2;
 				}
 			}
