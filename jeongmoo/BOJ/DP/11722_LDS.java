147f1c7078768753207ade160c1a93898dc63ca1
우정무 <app.le@kakao.com>
2022-04-06 09:30:53 +0900

    [BOJ] 가장 긴 감소하는 부분 수열 / 실버2 / 1분

---
+++ jeongmoo/BOJ/DP/11722_LDS.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 11722번. 가장 긴 감소하는 부분 수열
+public class BOJ11722_LDS {
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int n = Integer.parseInt(br.readLine());
+    	int[] data = new int[n];
+    	
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	for (int i = 0; i < n; i++) {
+			data[i] = Integer.parseInt(st.nextToken());
+		}
+    	
+    	// 계산
+    	int[] lis = new int[n];
+    	int size = Integer.MIN_VALUE;
+    	for (int i = 0; i < n; i++) {
+    		lis[i] = 1;
+    		for (int j = 0; j < i; j++) {
+				if (data[j] > data[i] && lis[j]+1 > lis[i])
+					lis[i] = lis[j]+1;
+			}
+    		size = Math.max(size, lis[i]);
+		}
+    	System.out.println(size);
+    }
+}
