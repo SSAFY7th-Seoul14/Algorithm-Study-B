f5a0238c9a3f7d8fba2a07425c0a19e0a602cbcc
안호진 <asdv94@naver.com>
2022-04-13 00:26:52 +0900

    [BOJ] N과 M (1) / S3 / 30분 https://www.acmicpc.net/problem/15649

---
+++ hojin/boj/backtracking/15649_N과M1.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15649_N과M1 {
+	static int n, m;
+	static int[] numbers, numbersM;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		numbers = new int[m];
+		permu(0, 0);
+		System.out.println(sb);
+		br.close();
+	}
+
+	private static void permu(int cnt, int flag) {
+		if (cnt == m) {
+			for (int i = 0; i < m; ++i) {
+				sb.append(numbers[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		for (int i = 1; i <= n; ++i) {
+			if ((flag & 1 << i) > 0)
+				continue;
+			numbers[cnt] = i;
+			permu(cnt + 1, flag | 1 << i);
+		}
+	}
+
+}
