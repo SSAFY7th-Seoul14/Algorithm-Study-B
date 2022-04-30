7fd62e817db8bf1c2308441e76c6380dde30f719
kotaehee <koxogml@naver.com>
2022-04-30 11:28:17 +0900

    [BOJ] 9935 문자열 폭발 / G4 / https://www.acmicpc.net/problem/9935

--- taehee/boj/문자열/9935.java
+++ taehee/boj/문자열/9935.java
@@ -7,30 +7,33 @@ public class Main {
 	
 	static String s;
 	static String b;
+	static Stack<Character> stk = new Stack<>();
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		s = br.readLine();
 		b = br.readLine();
 		
-		int i = 0;
-		while(i < s.length()) {
-			if(bomb(i)) {
-				s = s.substring(0, i) + s.substring(i+b.length());
-				i -= b.length()-1;
-				if(i<0) i = 0;
-			}
-			else {
-				i++;
+		for(int i = 0; i<s.length(); i++) {
+			stk.push(s.charAt(i));
+			if(stk.size() >= b.length() && bomb(i)) {
+				for (int j = 0; j < b.length(); j++) {
+					stk.pop();
+				}
 			}
 		}
-		System.out.println(s.length()!=0 ? s : "FRULA");
+		StringBuilder sb = new StringBuilder();
+		while(!stk.isEmpty()) {
+			sb.append(stk.pop());
+		}
+		sb.reverse();
+		System.out.println(sb.length()==0 ? "FRULA" : sb);
 	}
 
 	private static boolean bomb(int i) {
 		//중간에 하나라도 다르면 false, 다같으면 true
-		for (int j = i; j < i + b.length(); j++) {
-			if(s.charAt(j) != b.charAt(j-i))
+		for (int j = 0; j < b.length(); j++) {
+			if(stk.get(stk.size()- b.length() + j) != b.charAt(j))
 				return false;
 		} 
 		return true;
